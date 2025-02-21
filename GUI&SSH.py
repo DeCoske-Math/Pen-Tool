5f5937da0685265f7fbfba411ea04c9927435b8b
Downquark7 <mail2emn@gmail.com>
2025-02-21 16:12:52 -0600

    improve output formatting start logging all requests to log.tsv add log.tsv to gitignore

--- GUI&SSH.py
+++ GUI&SSH.py
@@ -11,16 +11,22 @@ Web File Exploit Test Tool
 import os
 import requests
 
-def processURL(url, index):
+def processURL(url, index, log):
 
+    # put all responses in dedicated folder and inside of that folder index numbers match the urls.txt line numbers
     path = "responses/" + str(index)
 
+    # skip url if folder of index exists, delete/move folder to refresh data
     if not os.path.exists(path):
 
         response = requests.get(url)
         os.makedirs(path)
 
-        print("Processing: " + str(index) + " " + url + " " + str(response.status_code) + " " + response.headers['Content-Type'])
+        print("Processing: " + str(index) + " " + str(response.status_code) + " " + response.headers.get('Content-Type', 'Unknown') + " " + url, end="")
+        log.write(str(index) + "\t" + str(response.status_code) + "\t" + response.headers.get('Content-Type', 'Unknown') + "\t" + url)
+
+        with open(path + "/content-type.txt", "w") as file:
+            file.write(response.headers.get('Content-Type', 'Unknown'))
 
         with open(path + "/response.txt", "w") as file:
             file.write(response.text)
@@ -31,16 +37,20 @@ def processURL(url, index):
         with open(path + "/status.txt", "w") as file:
             file.write(str(response.status_code))
     else:
-        print("Skipping: " + str(index) + " " + url)
+        print("Skipping:   " + str(index) + " " + url, end="")
 
 
 
 url_file = open("urls.txt", "r")
+log = open("log.tsv", "a")
 index = 1
 
+# index matches line number in urls.txt
 for line in url_file:
-    processURL(line, index)
+    processURL(line, index, log)
     index += 1
+
+log.write("\n")
 
 
 
