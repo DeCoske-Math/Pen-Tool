3a109f6ba61b6023a8fb7fe7141f2713f40fbdeb
Downquark7 <mail2emn@gmail.com>
2025-02-21 21:06:53 -0600

    add size limit add request timeout of 4s adjust print formatting handle all network request exceptions add timestamps to log file

--- GUI&SSH.py
+++ GUI&SSH.py
@@ -10,35 +10,53 @@ Web File Exploit Test Tool
 
 import os
 import requests
+from datetime import datetime
+
+
+def write_size_limited(file, data):
+    max_size = 1024 ** 3  # 1GB in bytes
+    if len(data.encode('utf-8')) < max_size:
+        file.write(data)
+    else:
+        print("Data exceeds 1GB and will not be written.")
+        file.write("Data exceeds 1GB and will not be written.")
+
 
 def processURL(url, index, log):
-
     # put all responses in dedicated folder and inside of that folder index numbers match the urls.txt line numbers
     path = "responses/" + str(index)
 
     # skip url if folder of index exists, delete/move folder to refresh data
     if not os.path.exists(path):
 
-        response = requests.get(url)
         os.makedirs(path)
 
-        print("Processing: " + str(index) + " " + str(response.status_code) + " " + response.headers.get('Content-Type', 'Unknown') + " " + url, end="")
-        log.write(str(index) + "\t" + str(response.status_code) + "\t" + response.headers.get('Content-Type', 'Unknown') + "\t" + url)
+        try:
+            response = requests.get(url, timeout=4)
+        except Exception as e:
+            print(f"{(type(e).__name__+':'):20}" + str(index) + " " + url, end="")
+            log.write(str(datetime.now()) + "\t" + str(index) + "\t" + "0" + "\t" + type(e).__name__ + "\t" + url)
+            return
+
+        print(f"{'Processing:':20}" + str(index) + " " + str(response.status_code) + " " + response.headers.get(
+            'Content-Type', 'Unknown') + " " + url, end="")
+        log.write(
+            str(datetime.now()) + "\t" + str(index) + "\t" + str(response.status_code) + "\t" + response.headers.get(
+                'Content-Type', 'Unknown') + "\t" + url)
 
         with open(path + "/content-type.txt", "w") as file:
-            file.write(response.headers.get('Content-Type', 'Unknown'))
+            write_size_limited(file, response.headers.get('Content-Type', 'Unknown'))
 
         with open(path + "/response.txt", "w") as file:
-            file.write(response.text)
+            write_size_limited(file, response.text)
 
         with open(path + "/headers.txt", "w") as file:
-            file.write(str(response.headers))
+            write_size_limited(file, str(response.headers))
 
         with open(path + "/status.txt", "w") as file:
-            file.write(str(response.status_code))
+            write_size_limited(file, str(response.status_code))
     else:
-        print("Skipping:   " + str(index) + " " + url, end="")
-
+        print(f"{'Skipping:':20}" + str(index) + " " + url, end="")
 
 
 url_file = open("urls.txt", "r")
